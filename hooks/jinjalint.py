8c4e0a56dcac3b39ab79af9dd26149ad82fb2438
Michael <dgmike@users.noreply.github.com>
2018-05-12 09:12:38 -0300

    Update jinjalint.py

--- hooks/jinjalint.py
+++ hooks/jinjalint.py
@@ -1,14 +1,20 @@
+from __future__ import print_function
+
 from jinjalint.lint import resolve_file_paths, lint
-import jinjalint
- 
+
+
 def parse_issues():
     files = resolve_file_paths('.', extensions=['.html'])
     lint = lint(files, {})
     return sorted(lint, key=lambda i: (i.location.file_path, i.location.line))
 
+
 def main():
-    issues = [
-        str(issue)
-        for issue in
-        parse_issues()
-    ]
+    issues = parse_issues()
+    for issue in issues:
+        print(str(issue))
+    return len(issues) == 0
+    
+
+if __name__ == '__main__':
+    sys.exit(check_json())
