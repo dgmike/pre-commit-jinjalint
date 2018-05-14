693bfbd129cbfd808d8ed71c592a221b71713cc3
Michael <michaelgranados@gmail.com>
2018-05-14 10:03:40 -0300

    correct args
    Signed-off-by: Michael <michaelgranados@gmail.com>

--- pre_commit_hooks/jinjalint_wrapper.py
+++ pre_commit_hooks/jinjalint_wrapper.py
@@ -3,24 +3,22 @@ from __future__ import print_function
 from jinjalint.lint import resolve_file_paths, lint
 import argparse
 
-def parse_issues():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'filenames',
-        nargs='.',
-        help='Filenames pre-commit believes are changed'
-    )
-    args = parser.parse_args(argv)
-
-    print(args.filenames)
-
-    files = resolve_file_paths(args.filenames, extensions=['.html'])
+def parse_issues(filenames):
+    files = resolve_file_paths(filenames, extensions=['.html'])
     lints = lint(files, {})
     return sorted(lints, key=lambda i: (i.location.file_path, i.location.line))
 
 
-def main():
-    issues = parse_issues()
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'filenames',
+        nargs='*',
+        help='Filenames pre-commit believes are changed'
+    )
+    args = parser.parse_args(argv)
+
+    issues = parse_issues(args.filenames)
     for issue in issues:
         print(str(issue))
     return int(len(issues) > 0)
